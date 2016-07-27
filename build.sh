f97a238d7c8346279b9b781e76cd36a90c421dce
Eric J. Ma <ericmajinglong@gmail.com>
2016-07-27 14:18:07 -0400

    added use of pandoc-tablenos to build script

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@ pandoc genomic-surveillance-whitepaper.md \
   -o "genomic-surveillance.pdf" \
   --latex-engine="/Library/TeX/texbin/xelatex" \
   --filter="/Users/ericmjl/anaconda/bin/pandoc-fignos" \
+  --filter="/Users/ericmjl/anaconda/bin/pandoc-tablenos" \
   --filter="/Users/ericmjl/anaconda/bin/pandoc-citeproc" \
   --bibliography="papers-library.bib" \
   --csl="pnas.csl" \
