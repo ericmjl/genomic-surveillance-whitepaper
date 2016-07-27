5e975f3cd118a28efe448fac81b3fbb8d49672cb
Eric J. Ma <ericmajinglong@gmail.com>
2016-07-27 10:22:32 -0400

    put output file name in quotation marks.

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 pandoc genomic-surveillance-whitepaper.md \
-  -o genomic-surveillance.pdf \
+  -o "genomic-surveillance.pdf" \
   --latex-engine="/Library/TeX/texbin/xelatex" \
   --filter="/Users/ericmjl/anaconda/bin/pandoc-fignos" \
   --filter="/Users/ericmjl/anaconda/bin/pandoc-citeproc" \
